01503fb1b3073618dd7235b2ebf654757b5ecae2
rickypcyt <105986682+rickypcyt@users.noreply.github.com>
2024-07-09 21:31:50 +0200

    Ya

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 // pages/index.tsx
+import styles from './Home.css'; // Importar estilos CSS
+import React from 'react';
+
 const HomePage = () => {
-    return (
-      <div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.leftBox}></div> {/* Cuadro a la izquierda */}
+      <div className={styles.content}>
         <h1>Hello, World!</h1>
-        <p>This is a simple Next.js page.</p>
+        <p>This is a simple Next.js page with a box on the left.</p>
       </div>
-    );
-  };
-  
-  export default HomePage;
-  
+    </div>
+  );
+};
+
+export default HomePage;
